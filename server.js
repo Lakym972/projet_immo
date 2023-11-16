740c25acd454328b11d92daaa21ae33d08762744
Lakym972 <132657810+Lakym972@users.noreply.github.com>
2023-11-16 10:13:42 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+require('dotenv').config();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'templates'));
+app.set('view engine', 'pug');
+
+app.get('/', function (req, res) {
+    res.render('index')
+});
+
+app.listen(process.env.PORT_HTTP, () => {
+    if (process.env.APP_ENV == 'dev') {
+        console.log(`Server is running on http://localhost:${process.env.PORT_HTTP}`);
+    }
+});
